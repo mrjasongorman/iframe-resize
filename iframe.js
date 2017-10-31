c662b0085da94183f33bdcc82994a61114e07a06
Jason Gorman <mrjasongorman@gmail.com>
2017-10-31 23:10:32 +0000

    added comments

--- iframe.js
+++ iframe.js
@@ -1,12 +1,12 @@
 (function(){
-  var theBody = document.getElementsByTagName('body')[0];
-  var currentHeight = theBody.offsetHeight;
+  var theBody = document.getElementsByTagName('body')[0]; // save body object in variable
+  var currentHeight = theBody.offsetHeight; // get the current height of the content
   setInterval(function(){
-    if( currentHeight === theBody.offsetHeight ){
+    if( currentHeight === theBody.offsetHeight ){ // check the content height hasn't changed, if not return immediately
       return;
     }else{
-      parent.postMessage( theBody.offsetHeight + 1, "*" );
-      currentHeight = theBody.offsetHeight;
+      parent.postMessage( theBody.offsetHeight + 1, "*" ); // if content height has changed send postmessage up to parent
+      currentHeight = theBody.offsetHeight; // save new content height to check again on the next loop
     }
-  }, 100);
+  }, 100); // iterate every 100ms giving an almost instant adjustment and avoiding secondary scroll bar flashing up
 })();
